097d05aa509d19e41daedf9ddc125ff484f32a1e
Danny Uzo <uzodinmadaniel42@gmail.com>
2024-08-30 17:39:42 +0100

    changes made to navigation component.

---
+++ src/app/(main)/_components/navigation.tsx
@@ -0,0 +1,7 @@
+export const Navigation = () => {
+    return (
+        <aside className="group/sidebar overflow-y-auto flex h-full justify-between flex-col z-[99999] border-2">
+Navigation
+        </aside>
+    )
+}
