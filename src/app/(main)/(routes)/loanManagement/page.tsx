1c18dbce84f919d843f4cfdbdc75f8be05ef3afd
Danny Uzo <uzodinmadaniel42@gmail.com>
2024-09-06 12:00:57 +0100

    updates: Multiple routes added

---
+++ src/app/(main)/(routes)/loanManagement/page.tsx
@@ -0,0 +1,9 @@
+const loanManagement = () => {
+    return (
+        <div className="w-full p-4 flex flex-col bg-[#0E0F1D] text-white ">
+            Loan Management Page
+        </div>
+    )
+};
+
+export default loanManagement;
---
+++ src/app/(main)/(routes)/orderManagement/page.tsx
@@ -0,0 +1,9 @@
+const orderManagement = () => {
+    return (
+        <div className="w-full p-4 flex flex-col bg-[#0E0F1D] text-white ">
+            order Management Page
+        </div>
+    )
+};
+
+export default orderManagement;
